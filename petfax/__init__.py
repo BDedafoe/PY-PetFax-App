b6318665379fb1992603068477960a35db8b9eef
BDedafoe <bmd6260@gmail.com>
2022-11-05 10:39:01 -0500

    "integrated pgAdmin"

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
+    # app.config['TEMPLATES_AUTO_RELOAD'] = True #
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Sodapop9@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    from . import models
+    models.db.init_app(app)            
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello(): 
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,30 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/new')
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        print(request.form)
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+
+    results = models.Fact.query.all()
+    
+    return render_template('facts/index.html', facts=results)
+
+@bp.route('/new', methods=['GET', 'POST'])
 def new(): 
-    return render_template('facts/new.html')
+    if request.method == "GET":
+        return render_template('facts/new.html')
+    elif request.method == "POST":
+        for key,value in request.form.items():
+            print(f"{key=} {value=}")    
+        return render_template("facts/index.html", form_accepted=True)
